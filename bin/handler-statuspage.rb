279abf58d2ee3972b8e5d6e45f98a998087ab93b
Andrew Thal <hello@athal7.com>
2017-03-09 16:23:26 -0500

    Update existing incidents on any change, not just on resolution

--- bin/handler-statuspage.rb
+++ bin/handler-statuspage.rb
@@ -55,16 +55,33 @@ class StatusPage < Sensu::Handler
   end
 
   def incident_status
-    desired_status = @event['check']["statuspage_incident_#{status_name}"]
-    if desired_status && ALLOWED_INCIDENT_STATUSES.include?(desired_status)
-      desired_status
-    else
-      DEFAULT_INCIDENT_STATUSES[status_name]
+    case @event['action']
+    when 'create'
+      desired_status = @event['check']["statuspage_incident_#{status_name}"]
+      if desired_status && ALLOWED_INCIDENT_STATUSES.include?(desired_status)
+        desired_status
+      else
+        DEFAULT_INCIDENT_STATUSES[status_name]
+      end
+    when 'resolve'
+      'resolved'
     end
   end
 
   def ignore_status?(status)
-    ["ignore", nil].include?(status)
+    ['ignore', nil].include?(status)
+  end
+
+  def description
+    @event['notification'] || [@event['client']['name'], @event['check']['name'], @event['check']['output']].join(' : ')
+  end
+
+  def incident_message(status)
+    if status == 'resolved'
+      "Problem with #{incident_key} has been resolved."
+    else
+      "There has been a problem: #{description}."
+    end
   end
 
   def handle
@@ -72,7 +89,6 @@ class StatusPage < Sensu::Handler
       page_id: settings['statuspage']['page_id'],
       api_key: settings['statuspage']['api_key']
     )
-    description = @event['notification'] || [@event['client']['name'], @event['check']['name'], @event['check']['output']].join(' : ')
     begin
       timeout(3) do
         if @event['check'].key?('component_id')
@@ -83,32 +99,27 @@ class StatusPage < Sensu::Handler
             )
           end
         end
-        response = case @event['action']
-                   when 'create'
-                     unless ignore_status?(incident_status)
-                       statuspage.create_realtime_incident(
-                         name: incident_key,
-                         status: incident_status,
-                         wants_twitter_update: 'f',
-                         message: "There has been a problem: #{description}."
-                       )
-                     end
-                   when 'resolve'
-                     incident_id = nil
-                     statuspage.get_all_incidents.each do |incident|
-                       if incident['name'] == incident_key
-                         incident_id = incident['id']
-                         break
-                       end
-                     end
+        existing_incident = statuspage.get_all_incidents.detect { |incident| incident['name'] == incident_key && incident['status'] != 'resolved' }
+        response = if existing_incident
                      statuspage.update_incident(
-                       name: "Problem with #{incident_key} has been resolved.",
+                       name: incident_key,
                        wants_twitter_update: 'f',
-                       status: 'resolved',
-                       incident_id: incident_id
+                       status: incident_status,
+                       incident_id: existing_incident['id'],
+                       message: incident_message(incident_status)
+                     )
+                   elsif !ignore_status?(incident_status)
+                     statuspage.create_realtime_incident(
+                       name: incident_key,
+                       status: incident_status,
+                       wants_twitter_update: 'f',
+                       message: incident_message(incident_status)
                      )
                    end
-        if ignore_status?(incident_status) || (response['status'] == incident_status && @event['action'] == 'create') || (response['status'] == 'resolved' && @event['action'] == 'resolve')
+        if ignore_status?(incident_status) ||
+           (response['status'] == incident_status && @event['action'] == 'create') ||
+           (response['status'] == 'resolved'      && @event['action'] == 'resolve')
+
           puts 'statuspage -- ' + @event['action'].capitalize + 'd incident -- ' + incident_key
         else
           puts 'statuspage -- failed to ' + @event['action'] + ' incident -- ' + incident_key
